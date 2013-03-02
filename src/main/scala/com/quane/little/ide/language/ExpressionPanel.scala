9b7658589577ed16808d41fb729a3e872cc09f9d
Sean Connolly <connolly.st@gmail.com>
2013-03-02 22:48:58 +0100

    Updated comments to reflect class (and project!) name changes.

--- src/main/scala/com/quane/little/ide/language/ExpressionPanel.scala
+++ src/main/scala/com/quane/little/ide/language/ExpressionPanel.scala
@@ -2,15 +2,16 @@ package com.quane.little.ide.language
 
 import scala.swing.Panel
 
-/** A Glass panel is a Scala Swing panel that represents a piece of the Glass
-  * programming language. Specifically, it is the 'view' component of the MVC
-  * framework whereby the Glass code itself is the 'model' and the
-  * {@code GlassPanelController} is, as the name suggests, the 'controller'. As
-  * such, a Glass panel is responsible for displaying an appropriate interface
-  * for the Glass code it fronts; both in terms of readability and
-  * manipulation. All changes to the Glass code are handled by the controller.
+/** An expression panel is a Scala Swing panel that represents a piece of the
+  * little programming language. Specifically, it is the 'view' component of
+  * the MVC framework whereby the little code itself is the 'model' and the
+  * {@code ExpressionPanelController} is, as the name suggests, the
+  * 'controller'. As such, an expression panel is responsible for displaying
+  * an appropriate interface for the little code it fronts; both in terms of
+  * readability and manipulation. All changes to the little code are handled
+  * by the controller.
   *
   * @author Sean Connolly
   */
 trait ExpressionPanel
-        extends Panel
+    extends Panel
