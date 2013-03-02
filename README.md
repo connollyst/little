181feccb08bc05687661f5822f564b30493f711d
Sean Connolly <connolly.st@gmail.com>
2013-03-02 22:37:45 +0100

    Moved TODO list to the readme file.

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
-# Glass
+TODO
+X add toolkit on left
+X add workspace on right
+X add elements to toolkit
+X d&d elements from toolkit to workspace
+X d&d elements into programs
+X compile programs to expressions
+X add game into ide
+X add menu to run game
 
-Wicked cool.
+V1:
+X OnSpawn - Move forward
+X OnContact - Turn Randomly
+X OnCloseToFood - Go Toward Food
+X OnGotFood - Stop
+V2:
+- Backed by GUI
+V3:
+- Other objects.. can bounce off anything
+V4:
+- Remove Walls.
+- Scrolling - limited distance from dude
+- Large World - not procedural ATM
+
+- game always running
+- push changes to game on 'Run'
+- game follows guy 
+
+
+
+
+~ add support for other events
+- add support for multiple guys
+- add support for obstacles
+- add speech bubbles
+- add food
+- save and load programs
+- add better IDE controls
+- add better memory handling (types, limit, visualization, etc.)
+- code should be time controlled (eg one Expression per frame)
+- add camera scrolling, tracking, etc.
--- src/main/scala/com/quane/little/ide/IDE.scala
+++ src/main/scala/com/quane/little/ide/IDE.scala
@@ -10,47 +10,6 @@ import org.eintr.loglady.Logging
 import com.google.common.eventbus.EventBus
 import com.google.common.eventbus.Subscribe
 
-// TODO
-// X add toolkit on left
-// X add workspace on right
-// X add elements to toolkit
-// X d&d elements from toolkit to workspace
-// X d&d elements into programs
-// X compile programs to expressions
-// X add game into ide
-// X add menu to run game
-
-// V1:
-// OnSpawn - Move forward
-// OnContact - Turn Randomly
-// OnCloseToFood - Go Toward Food
-// OnGotFood - Stop
-// V2:
-// Backed by GUI
-// V3:
-// Other objects.. can bounce off anything
-// V4:
-// Remove Walls.
-// Scrolling - limited distance from dude
-// Large World - not procedural ATM
-
-// - game always running
-// - push changes to game on 'Run'
-// - game follows guy 
-
-
-
-
-// ~ add support for other events
-// - add support for multiple guys
-// - add support for obstacles
-// - add speech bubbles
-// - add food
-// - save and load programs
-// - add better IDE controls
-// - add better memory handling (types, limit, visualization, etc.)
-// - code should be time controlled (eg one Expression per frame)
-// - add camera scrolling, tracking, etc.
 object IDE extends SimpleSwingApplication {
 
     // Set the window title in Mac 
