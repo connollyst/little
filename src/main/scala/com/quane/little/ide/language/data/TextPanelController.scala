05b8bbc7bae9070b0ff3e52514960adfea62a872
Sean Connolly <connolly.st@gmail.com>
2013-03-04 02:18:30 +0100

    TextPanelController now reacts to drop events of GetTextStatement only.

--- src/main/scala/com/quane/little/ide/language/data/TextPanelController.scala
+++ src/main/scala/com/quane/little/ide/language/data/TextPanelController.scala
@@ -1,39 +1,45 @@
 package com.quane.little.ide.language.data
 
 import scala.swing.Reactor
-
 import org.eintr.loglady.Logging
-
+import com.quane.little.ide.StepAddedEvent
 import com.quane.little.ide.language.ExpressionPanelController
 import com.quane.little.language.Scope
 import com.quane.little.language.data.Text
+import com.quane.little.language.Expression
+import com.quane.little.ide.GetTextStatementAddedEvent
 
 abstract class TextPanelController(override val view: TextPanel)
-        extends ExpressionPanelController[Text](view)
+        extends ExpressionPanelController[Expression[Text]](view)
         with Reactor
         with Logging {
 
     override def validate {
         log.error("TODO: implement validate")
     }
-
 }
 
 class TextFieldPanelController(override val view: TextFieldPanel)
         extends TextPanelController(view) {
 
-    override def compile(scope: Scope): Text = {
+    override def compile(scope: Scope): Expression[Text] = {
         val text = view.value
         return new Text(text)
     }
-
 }
 
 class TextExpressionPanelController(override val view: TextExpressionPanel)
         extends TextPanelController(view) {
 
-    override def compile(scope: Scope): Text = {
-        // TODO get Expression
-        return new Text("todo")
+    var expression = None: Option[ExpressionPanelController[Expression[Text]]]
+
+    override def compile(scope: Scope): Expression[Text] = {
+        expression.get.compile(scope);
+    }
+
+    listenTo(view)
+    reactions += {
+        case event: GetTextStatementAddedEvent =>
+            expression = Option(event.controller)
     }
 }
