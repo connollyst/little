0b1da180343a005509fca7750e1cee0890871890
Sean Connolly <connolly.st@gmail.com>
2013-03-04 02:17:30 +0100

    Added more specific events: GetterAdded, GetTextStatementAdded, etc.

--- src/main/scala/com/quane/little/ide/StepAddedEvent.scala
+++ src/main/scala/com/quane/little/ide/StepAddedEvent.scala
@@ -1,10 +1,14 @@
 package com.quane.little.ide
 
 import scala.swing.event.Event
-import com.quane.little.language.Expression
 import com.quane.little.ide.language.ExpressionPanelController
-import com.quane.little.ide.language.ListenerPanelController
 import com.quane.little.ide.language.FunctionPanelController
+import com.quane.little.ide.language.GetStatementPanelController
+import com.quane.little.ide.language.ListenerPanelController
+import com.quane.little.ide.language.SetStatementPanelController
+import com.quane.little.language.Expression
+import com.quane.little.language.data.Value
+import com.quane.little.language.data.Text
 
 class StepAddedEvent(val controller: ExpressionPanelController[Expression[Any]])
     extends Event
@@ -19,4 +23,22 @@ class FunctionAddedEvent(override val controller: FunctionPanelController,
                          val toolType: ToolType,
                          val x: Int,
                          val y: Int)
+        extends StepAddedEvent(controller)
+
+class GetterAddedEvent[V <: Value](override val controller: GetStatementPanelController[V],
+                                   val toolType: ToolType,
+                                   val x: Int,
+                                   val y: Int)
+        extends StepAddedEvent(controller)
+
+class GetTextStatementAddedEvent(override val controller: GetStatementPanelController[Text],
+                                 override val toolType: ToolType,
+                                 override val x: Int,
+                                 override val y: Int)
+        extends GetterAddedEvent[Text](controller, toolType, x, y)
+
+class SetterAddedEvent[V <: Value](override val controller: SetStatementPanelController[V],
+                                   val toolType: ToolType,
+                                   val x: Int,
+                                   val y: Int)
         extends StepAddedEvent(controller)
