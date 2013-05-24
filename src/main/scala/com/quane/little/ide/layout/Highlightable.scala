c2eabce9ffa535b17505800e3847512c0bf6f184
Sean Connolly <connolly.st@gmail.com>
2013-05-24 21:34:03 +0200

    Enabled highlighting.

--- src/main/scala/com/quane/little/ide/layout/Highlightable.scala
+++ src/main/scala/com/quane/little/ide/layout/Highlightable.scala
@@ -2,18 +2,30 @@ package com.quane.little.ide.layout
 
 import javafx.scene.Node
 
+object Highlightable {
+
+  private val highlighted = "highlight"
+
+  private val unhighlighted = "no-highlight"
+
+}
+
 /**
+ * A [[javafx.scene.Node]] which can be highlighted.
+ * A highlighted node has a bright border.
  *
  * @author Sean Connolly
  */
 trait Highlightable extends Node {
 
   def highlight() {
-    // TODO highlight
+    getStyleClass.remove(Highlightable.unhighlighted)
+    getStyleClass.add(Highlightable.highlighted)
   }
 
   def unhighlight() {
-    // TODO unhighlight
+    getStyleClass.remove(Highlightable.highlighted)
+    getStyleClass.add(Highlightable.unhighlighted)
   }
 
 }
