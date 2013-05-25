65723d2518c5c52e8dd797a20548d1f354108ab5
Sean Connolly <connolly.st@gmail.com>
2013-05-25 15:59:21 +0200

    Removed old Swing 'statement' panels and controllers.

--- src/main/scala/com/quane/little/ide/language/statement/StatementController.scala
+++
@@ -1,9 +0,0 @@
-package com.quane.little.ide.language.statement
-
-import com.quane.little.ide.language.ExpressionController
-import com.quane.little.language.Statement
-
-/** @author Sean Connolly
-  */
-abstract class StatementController[S <: Statement](override val view: StatementPanel)
-    extends ExpressionController[S](view)
