5f2050a4892ccb373fb11b298c98b0bd6ce82be8
Sean Connolly <connolly.st@gmail.com>
2014-01-11 18:02:50 +0100

    Click StepEmpty to add Step.

---
+++ little-ide/src/main/java/com/quane/little/view/LittleStep.java
@@ -0,0 +1,13 @@
+package com.quane.little.view;
+
+import com.vaadin.ui.HorizontalLayout;
+
+public class LittleStep extends HorizontalLayout {
+
+	private static final String STYLE = "l-step";
+
+	public LittleStep() {
+		setStyleName(STYLE);
+	}
+
+}
--- little-ide/src/main/java/com/quane/little/view/LittleStepEmpty.java
+++ little-ide/src/main/java/com/quane/little/view/LittleStepEmpty.java
@@ -6,19 +6,23 @@ import com.vaadin.ui.CssLayout;
 
 public class LittleStepEmpty extends CssLayout {
 
-	private static final String STYLE = "l-step l-step-empty";
+	private static final String STYLE = "l-step-empty";
 
 	public LittleStepEmpty() {
 		setStyleName(STYLE);
 		addLayoutClickListener(new LittleStepEmptyClickListener());
 	}
 
-	private static final class LittleStepEmptyClickListener implements
+	private final class LittleStepEmptyClickListener implements
 			LayoutClickListener {
 
 		@Override
 		public void layoutClick(LayoutClickEvent event) {
 			System.out.println("Adding a new step!");
+			LittleStepEmpty stepEmpty = LittleStepEmpty.this;
+			LittleStepList stepList = (LittleStepList) stepEmpty.getParent();
+			int stepCount = stepList.getComponentCount();
+			stepList.addComponent(new LittleStep(), stepCount - 1);
 		}
 
 	}
