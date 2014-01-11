12785b9437cd6454f049eed68bfabf92ef984375
Sean Connolly <connolly.st@gmail.com>
2014-01-11 17:54:19 +0100

    Added click listener for empty Step.

--- little-ide/src/main/java/com/quane/little/view/LittleStepEmpty.java
+++ little-ide/src/main/java/com/quane/little/view/LittleStepEmpty.java
@@ -1,13 +1,25 @@
 package com.quane.little.view;
 
-import com.vaadin.ui.Label;
+import com.vaadin.event.LayoutEvents.LayoutClickEvent;
+import com.vaadin.event.LayoutEvents.LayoutClickListener;
+import com.vaadin.ui.CssLayout;
 
-public class LittleStepEmpty extends Label {
+public class LittleStepEmpty extends CssLayout {
 
 	private static final String STYLE = "l-step l-step-empty";
 
 	public LittleStepEmpty() {
 		setStyleName(STYLE);
+		addLayoutClickListener(new LittleStepEmptyClickListener());
 	}
 
+	private static final class LittleStepEmptyClickListener implements
+			LayoutClickListener {
+
+		@Override
+		public void layoutClick(LayoutClickEvent event) {
+			System.out.println("Adding a new step!");
+		}
+
+	}
 }
