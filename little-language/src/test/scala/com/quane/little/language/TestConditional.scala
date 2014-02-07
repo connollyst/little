ae5627e16a049ba2be435ea3f114fa0b116655d5
Sean Connolly <connolly.st@gmail.com>
2014-02-07 23:23:45 +0100

    Removed obviated Event tests & implemented Conditional tests.

--- little-language/src/test/scala/com/quane/little/language/TestConditional.scala
+++ little-language/src/test/scala/com/quane/little/language/TestConditional.scala
@@ -1,34 +1,39 @@
 package com.quane.little.language
 
-import com.quane.little.language.data.Value
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfter, FunSuite}
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
+import com.quane.little.language.data.Value
+import com.quane.little.language.memory.Pointer
 
 @RunWith(classOf[JUnitRunner])
-class TestConditional extends FunSuite {
+class TestConditional extends FunSuite with BeforeAndAfter {
 
-  val pos = new Value(true)
-  val neg = new Value(false)
+  private val name = "VariableName"
+  private val value = "VariableValue"
+  private val positive = new Value(true)
+  private val negative = new Value(false)
 
-  test("test positive conditional") {
-    val func = new Function().addStep(
-      new PrintStatement(new Value("Sean is Cool!"))
-    )
-    val cond = new Conditional(pos, func)
-    cond.evaluate
-    // TODO assert function was evaluated
-    fail("TODO implement test")
+  /** The conditional test evaluates to true; the expression should be evaluated.
+    */
+  test("test condition evaluation: positive") {
+    val fun = createTestFunction
+    new Conditional(positive, fun).evaluate
+    assert(fun.fetch(name).value.asText == value)
   }
 
-  test("test negative conditional") {
-    val func = new Function().addStep(
-      new PrintStatement(new Value("Sean is Cool!"))
-    )
-    val cond = new Conditional(neg, func)
-    cond.evaluate
-    // TODO assert function was NOT evaluated
-    fail("TODO implement test")
+  /** The conditional test evaluates to false; the expression should not be evaluated.
+    */
+  test("test condition evaluation: negative") {
+    val fun = createTestFunction
+    new Conditional(negative, fun).evaluate
+    assert(fun.fetch(name).value.asText != value)
+  }
+
+  private def createTestFunction = {
+    val fun = new Function(null)
+    val pointer = new Pointer(fun, name)
+    fun.addStep(new SetStatement(pointer, new Value(value)))
   }
 
 }
