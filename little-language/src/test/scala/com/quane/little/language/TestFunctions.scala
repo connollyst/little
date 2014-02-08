24c8713775b3da4a9f263e55c3a16bb66312c38c
Sean Connolly <connolly.st@gmail.com>
2014-02-08 20:07:36 +0100

    Removed warnings in assertions.

--- little-language/src/test/scala/com/quane/little/language/TestFunctions.scala
+++ little-language/src/test/scala/com/quane/little/language/TestFunctions.scala
@@ -43,32 +43,32 @@ class TestFunctions extends FunSuite {
 
   test("get angle to from (1,1) to (2,1) should be 0 degrees") {
     val guy = new Operator(new Runtime, new StubOperable(1f, 1f))
-    val angle = Functions.getAngleTo(guy, new Value(2), new Value(1)).evaluate
-    assert(angle == 0, "expected 0, actual=" + angle.primitive)
+    val angle = Functions.getAngleTo(guy, new Value(2), new Value(1)).evaluate.asInt
+    assert(angle == 0, "expected 0, actual=" + angle)
   }
 
   test("get angle to from (1,1) to (1,2) should be 90 degrees") {
     val guy = new Operator(new Runtime, new StubOperable(1f, 1f))
-    val angle = Functions.getAngleTo(guy, new Value(1), new Value(2)).evaluate
-    assert(angle == 90, "expected 90, actual=" + angle.primitive)
+    val angle = Functions.getAngleTo(guy, new Value(1), new Value(2)).evaluate.asInt
+    assert(angle == 90, "expected 90, actual=" + angle)
   }
 
   test("get angle to from (0,0) to (1,1) should be 45 degrees") {
     val guy = new Operator(new Runtime, new StubOperable(0f, 0f))
-    val angle = Functions.getAngleTo(guy, new Value(1), new Value(1)).evaluate
-    assert(angle == 45, "expected 45, actual=" + angle.primitive)
+    val angle = Functions.getAngleTo(guy, new Value(1), new Value(1)).evaluate.asInt
+    assert(angle == 45, "expected 45, actual=" + angle)
   }
 
   test("get angle to from (1,1) to (2,2) should be 45 degrees") {
     val guy = new Operator(new Runtime, new StubOperable(1f, 1f))
-    val angle = Functions.getAngleTo(guy, new Value(2), new Value(2)).evaluate
-    assert(angle == 45, "expected 45, actual=" + angle.primitive)
+    val angle = Functions.getAngleTo(guy, new Value(2), new Value(2)).evaluate.asInt
+    assert(angle == 45, "expected 45, actual=" + angle)
   }
 
   test("get angle to from (1,1) to (-1,-1) should be 225 degrees") {
     val guy = new Operator(new Runtime, new StubOperable(1f, 1f))
-    val angle = Functions.getAngleTo(guy, new Value(-1), new Value(-1)).evaluate
-    assert(angle == 225, "expected 225, actual=" + angle.primitive)
+    val angle = Functions.getAngleTo(guy, new Value(-1), new Value(-1)).evaluate.asInt
+    assert(angle == 225, "expected 225, actual=" + angle)
   }
 
   test("point from (0,0) to (1,0) should set direction to 0 degrees") {
