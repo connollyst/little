e708ac52ef10f3701c6ac5e548ba733f9dd672d1
Sean Connolly <connolly.st@gmail.com>
2014-02-08 19:13:49 +0100

    Implemented more BasicMath operations.

--- little-language/src/main/scala/com/quane/little/language/math/BasicMath.scala
+++ little-language/src/main/scala/com/quane/little/language/math/BasicMath.scala
@@ -5,17 +5,56 @@ import com.quane.little.language.data.Value
 import org.eintr.loglady.Logging
 
 sealed trait BasicMath
-  extends Expression[Value]
+  extends Math
 
-class Addition(a: Expression[Value], b: Expression[Value])
+class Addition(a: Expression, b: Expression)
   extends BasicMath
   with Logging {
 
   def evaluate: Value = {
-    val numberA = a.evaluate.asNumber
-    val numberB = b.evaluate.asNumber
-    log.info("Adding " + numberA + " to " + numberB)
+    val numberA = a.evaluate.asDouble
+    val numberB = b.evaluate.asDouble
+    log.info("Adding " + numberB + " to " + numberA)
     new Value(numberA + numberB)
+  }
+
+}
+
+class Subtraction(a: Expression, b: Expression)
+  extends BasicMath
+  with Logging {
+
+  def evaluate: Value = {
+    val numberA = a.evaluate.asDouble
+    val numberB = b.evaluate.asDouble
+    log.info("Subtracting " + numberB + " from " + numberA)
+    new Value(numberA - numberB)
+  }
+
+}
+
+class Multiplication(a: Expression, b: Expression)
+  extends BasicMath
+  with Logging {
+
+  def evaluate: Value = {
+    val numberA = a.evaluate.asDouble
+    val numberB = b.evaluate.asDouble
+    log.info("Multiplying " + numberA + " by " + numberB)
+    new Value(numberA * numberB)
+  }
+
+}
+
+class Division(a: Expression, b: Expression)
+  extends BasicMath
+  with Logging {
+
+  def evaluate: Value = {
+    val numberA = a.evaluate.asDouble
+    val numberB = b.evaluate.asDouble
+    log.info("Dividing " + numberA + " by " + numberB)
+    new Value(numberA / numberB)
   }
 
 }
---
+++ little-language/src/main/scala/com/quane/little/language/math/Math.scala
@@ -0,0 +1,7 @@
+package com.quane.little.language.math
+
+import com.quane.little.language.Expression
+import com.quane.little.language.data.Value
+
+trait Math
+  extends Expression
