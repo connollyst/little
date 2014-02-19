de077b2afb2385a095aa6df56c0c572b789bc002
Sean Connolly <connolly.st@gmail.com>
2014-02-19 01:20:24 +0100

    notes

--- little-ide/core/src/main/scala/com/quane/little/ide/presenter/ConditionalPresenter.scala
+++ little-ide/core/src/main/scala/com/quane/little/ide/presenter/ConditionalPresenter.scala
@@ -6,25 +6,25 @@ import com.quane.little.language.{Conditional, Expression, Scope}
 class ConditionalPresenter[V <: ConditionalView](view: V)
   extends ExpressionPresenter with ConditionalViewListener {
 
-  private val _test: ExpressionPresenter = view.setConditionStatement()
+  private val _condition: ExpressionPresenter = view.setConditionStatement()
   private val _thenBlock: BlockPresenter[_] = view.setThenBlock()
   private val _elseBlock: BlockPresenter[_] = view.setElseBlock()
 
   view.addViewListener(this)
 
-  private[presenter] def test_=(test: Expression): Unit = {
-    // TODO what happens here?
-    _test
+  private[presenter] def condition_=(condition: Expression): Unit = {
+    // TODO i think we need to create a view depending on the expression type?
+    _condition
   }
 
   def setSteps[E <: Expression](steps: List[E]): Unit = {
-    println("Setting conditional steps: " + steps)
+    println("Setting conditional THEN steps: " + steps)
     _thenBlock.setSteps(steps)
   }
 
   override def compile(scope: Scope): Expression = {
     // TODO the else block is not supported yet!!!
-    new Conditional(_test.compile(scope), _thenBlock.compile(scope))
+    new Conditional(_condition.compile(scope), _thenBlock.compile(scope))
   }
 
 }
