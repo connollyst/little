7a48a2fc0ce45bf852321be2bfd1510bce27bfb0
Sean Connolly <connolly.st@gmail.com>
2014-02-20 00:22:19 +0100

    toolbox presenter tests

---
+++ little-ide/core/src/test/scala/com/quane/little/ide/presenter/TestToolboxPresenter.scala
@@ -0,0 +1,19 @@
+package com.quane.little.ide.presenter
+
+import org.scalatest.FunSuite
+import org.scalatest.mock.MockitoSugar
+import com.quane.little.ide.view.ToolboxView
+import org.mockito.Mockito._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TestToolboxPresenter extends FunSuite with MockitoSugar {
+
+  test("test listener registered") {
+    val view = mock[ToolboxView]
+    val presenter = new ToolboxPresenter(view)
+    verify(view).addViewListener(presenter)
+  }
+
+}
