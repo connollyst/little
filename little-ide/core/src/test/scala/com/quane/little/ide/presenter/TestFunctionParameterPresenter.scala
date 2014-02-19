10e9b601fdf40205d5f8ed111534707c07c60eed
Sean Connolly <connolly.st@gmail.com>
2014-02-19 01:40:32 +0100

    added tests for FunctionParameterPresenter

---
+++ little-ide/core/src/test/scala/com/quane/little/ide/presenter/TestFunctionParameterPresenter.scala
@@ -0,0 +1,26 @@
+package com.quane.little.ide.presenter
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import com.quane.little.ide.view.FunctionParameterView
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito._
+
+@RunWith(classOf[JUnitRunner])
+class TestFunctionParameterPresenter extends FunSuite with MockitoSugar {
+
+  test("test listener registered") {
+    val view = mock[FunctionParameterView]
+    val presenter = new FunctionParameterPresenter(view)
+    verify(view).addViewListener(presenter)
+  }
+
+  test("test name propagates to view") {
+    val view = mock[FunctionParameterView]
+    val presenter = new FunctionParameterPresenter(view)
+    presenter.name = "sean is cool"
+    verify(view).setName("sean is cool")
+  }
+
+}
