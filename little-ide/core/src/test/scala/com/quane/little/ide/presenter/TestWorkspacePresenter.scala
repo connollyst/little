b6ece72861e0ce352fa4577510a8b6cfdbb4b229
Sean Connolly <connolly.st@gmail.com>
2014-02-19 23:19:00 +0100

    basic tests for workspace presenter

---
+++ little-ide/core/src/test/scala/com/quane/little/ide/presenter/TestWorkspacePresenter.scala
@@ -0,0 +1,19 @@
+package com.quane.little.ide.presenter
+
+import org.scalatest.FunSuite
+import org.scalatest.mock.MockitoSugar
+import com.quane.little.ide.view.WorkspaceView
+import org.mockito.Mockito._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TestWorkspacePresenter extends FunSuite with MockitoSugar {
+
+  test("test listener registered") {
+    val view = mock[WorkspaceView]
+    val presenter = new WorkspacePresenter(view)
+    verify(view).addViewListener(presenter)
+  }
+
+}
