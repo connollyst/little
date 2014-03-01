113eb6a61f738b4c8deb57628533c193fc71c4b6
Sean Connolly <connolly.st@gmail.com>
2014-03-01 23:27:44 +0100

    added a placeholder service for fetching functions

---
+++ little-ide/core/src/main/scala/com/quane/little/ide/model/FunctionService.scala
@@ -0,0 +1,35 @@
+package com.quane.little.ide.model
+
+import com.quane.little.language.{FunctionReference, Functions, FunctionDefinition, Runtime}
+import com.quane.little.language.data.Value
+
+/** A service for interacting with [[com.quane.little.language.Functions]].
+  *
+  * @author Sean Connolly
+  */
+object FunctionService {
+
+  def fetchReference(name: String): FunctionReference = {
+    val definition = fetchDefinition(name)
+    val reference = new FunctionReference(new Runtime, definition.name)
+    definition.params foreach {
+      param =>
+      // TODO parameters should have default values
+        reference.addArg(param.name, new Value(""))
+    }
+    reference
+  }
+
+  def fetchDefinition(name: String): FunctionDefinition = {
+    name match {
+      case "blank" => Functions.blank
+      case "move" => Functions.move
+      case "stop" => Functions.stop
+      case "turn" => Functions.turn
+      case "voyage" => Functions.voyage
+      case "print dir" => Functions.printDirection
+      case _ => throw new IllegalArgumentException("Unknown function: '" + name + "'")
+    }
+  }
+
+}
