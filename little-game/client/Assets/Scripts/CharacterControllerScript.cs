9d1175b49294783b015c512a865cd4406db6ca79
Sean Connolly <connolly.st@gmail.com>
2014-03-08 20:33:09 +0100

    Unity client skeleton.

---
+++ little-game/client/Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+
+		private float maxSpeed = 10f;
+		private bool facingRight = true;
+		private Animator anim;
+
+		// Use this for initialization
+		void Start ()
+		{
+				anim = GetComponent<Animator> ();
+		}
+	
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+				float move = Input.GetAxis ("Horizontal");
+				anim.SetFloat ("Speed", Mathf.Abs (move));
+				rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		}
+
+}
