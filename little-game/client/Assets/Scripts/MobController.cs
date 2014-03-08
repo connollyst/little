f4d841a4aa1eaaf60b96d942d7b5694bf84c8f7d
Sean Connolly <connolly.st@gmail.com>
2014-03-09 00:12:14 +0100

    Changed CharacterControllerScript name.

---
+++ little-game/client/Assets/Scripts/MobController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Sfs2X;
+
+public class MobController : MonoBehaviour
+{
+
+		private float maxSpeed = 10f;
+		private Animator anim;
+		private SmartFox smartFox;
+
+		void Start ()
+		{
+				anim = GetComponent<Animator> ();
+				smartFox = SmartFoxConnection.Connection;
+				if (smartFox == null) {
+						Debug.LogError ("Not connected to server.");
+				}
+		}
+
+		void FixedUpdate ()
+		{
+				if (smartFox != null) {
+						smartFox.ProcessEvents ();
+				}
+				float move = Input.GetAxis ("Horizontal");
+				anim.SetFloat ("Speed", Mathf.Abs (move));
+				rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		}
+
+
+}
