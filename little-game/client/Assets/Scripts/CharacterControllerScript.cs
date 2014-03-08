189c1fd996eb559cda6ceedb83b5bf17c8e2fbc7
Sean Connolly <connolly.st@gmail.com>
2014-03-08 23:09:43 +0100

    Added SmartFox API dll.

--- little-game/client/Assets/Scripts/CharacterControllerScript.cs
+++ little-game/client/Assets/Scripts/CharacterControllerScript.cs
@@ -1,25 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using Sfs2X;
+using Sfs2X.Core;
+using Sfs2X.Requests;
+
 public class CharacterControllerScript : MonoBehaviour
 {
-
+		public string serverName = "127.0.0.1";
+		public int serverPort = 9933;
 		private float maxSpeed = 10f;
-		private bool facingRight = true;
 		private Animator anim;
+		private SmartFox smartFox;
 
-		// Use this for initialization
 		void Start ()
 		{
 				anim = GetComponent<Animator> ();
+				smartFox = new SmartFox (true);
+				Debug.Log ("API Version: " + smartFox.Version);
+		
+				// Register callback delegate
+				smartFox.AddEventListener (SFSEvent.CONNECTION, OnConnection);
+				smartFox.AddEventListener (SFSEvent.CONNECTION_LOST, OnConnectionLost);
+				smartFox.AddEventListener (SFSEvent.LOGIN, OnLogin);
+				smartFox.AddEventListener (SFSEvent.LOGIN_ERROR, OnLoginError);
+				smartFox.AddEventListener (SFSEvent.ROOM_JOIN, OnRoomJoin);
+		
+				smartFox.Connect (serverName, serverPort);
 		}
-	
-		// Update is called once per frame
+
 		void FixedUpdate ()
 		{
+				smartFox.ProcessEvents ();
 				float move = Input.GetAxis ("Horizontal");
 				anim.SetFloat ("Speed", Mathf.Abs (move));
 				rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		}
+
+		public void OnConnection (BaseEvent evt)
+		{
+				bool success = (bool)evt.Params ["success"];
+				string error = (string)evt.Params ["errorMessage"];
+				Debug.Log ("On Connection callback got: " + success + " (error : <" + error + ">)");
+				if (success) {
+						SmartFoxConnection.Connection = smartFox;
+						Debug.Log ("Connection succesful, logging in..");
+						smartFox.Send (new LoginRequest ("", "", "little"));
+				} else {
+						Debug.LogError ("Can't connect to server!");
+				}
+		}
+
+		public void OnConnectionLost (BaseEvent evt)
+		{
+				Debug.Log ("Connection was lost, Reason: " + (string)evt.Params ["reason"]);
+		}
+	
+		public void OnLogin (BaseEvent evt)
+		{
+				Debug.Log ("Logged in successfully, joining room..");
+				smartFox.Send (new JoinRoomRequest ("LittleTest"));
+		}
+	
+		public void OnLoginError (BaseEvent evt)
+		{
+				Debug.Log ("Login error: " + (string)evt.Params ["errorMessage"]);
+		}
+
+		public void OnRoomJoin (BaseEvent evt)
+		{
+				Debug.Log ("Joined room successfully");
 		}
 
 }
