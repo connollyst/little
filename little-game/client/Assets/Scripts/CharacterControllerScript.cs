a7a0fe92dd96bf56b03c313933eb28817ee7a615
Sean Connolly <connolly.st@gmail.com>
2014-03-09 00:08:03 +0100

    Process SmartFox events every step.

--- little-game/client/Assets/Scripts/CharacterControllerScript.cs
+++ little-game/client/Assets/Scripts/CharacterControllerScript.cs
@@ -20,6 +20,7 @@ public class CharacterControllerScript : MonoBehaviour
 
 		void FixedUpdate ()
 		{
+				smartFox.ProcessEvents ();
 				float move = Input.GetAxis ("Horizontal");
 				anim.SetFloat ("Speed", Mathf.Abs (move));
 				rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
