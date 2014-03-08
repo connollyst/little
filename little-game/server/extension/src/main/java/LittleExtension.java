af96624c2241e9379d81f631a8d753f244196c60
Sean Connolly <connolly.st@gmail.com>
2014-03-08 16:35:11 +0100

    Added 'hello world' SmartFoxServer extension.

---
+++ little-game/server/extension/src/main/java/LittleExtension.java
@@ -0,0 +1,15 @@
+import com.smartfoxserver.v2.extensions.SFSExtension;
+
+/**
+ * The SmartFox server 'extension' for the little game.
+ *
+ * @author Sean Connolly
+ */
+public class LittleExtension extends SFSExtension {
+
+	@Override
+	public void init() {
+		trace("Hello Little World!");
+	}
+
+}
