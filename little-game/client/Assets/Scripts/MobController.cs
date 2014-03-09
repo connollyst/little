fd584a591ccae90aa2012cff00af976095cab11c
Sean Connolly <connolly.st@gmail.com>
2014-03-09 01:35:49 +0100

    Character created by prefab and synced to server.

--- little-game/client/Assets/Scripts/MobController.cs
+++ little-game/client/Assets/Scripts/MobController.cs
@@ -1,31 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-using Sfs2X;
-
 public class MobController : MonoBehaviour
 {
 
 		private float maxSpeed = 10f;
 		private Animator anim;
-		private SmartFox smartFox;
 
 		public bool MovementDirty { get; set; }
 
 		void Start ()
 		{
 				anim = GetComponent<Animator> ();
-				smartFox = SmartFoxConnection.Connection;
-				if (smartFox == null) {
-						Debug.LogError ("Not connected to server.");
-				}
 		}
 
 		void FixedUpdate ()
 		{
-				if (smartFox != null) {
-						smartFox.ProcessEvents ();
-				}
 				float y = Input.GetAxis ("Vertical");
 				float x = Input.GetAxis ("Horizontal");
 				if (y != 0 || x != 0) {
