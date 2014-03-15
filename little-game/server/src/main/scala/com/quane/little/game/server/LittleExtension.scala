cbb47c42ebacb30d36c552f03e50c9ee75e9a278
Sean Connolly <connolly.st@gmail.com>
2014-03-15 14:09:07 +0100

    Engine is updated on initialization.

--- little-game/server/src/main/scala/com/quane/little/game/server/LittleExtension.scala
+++ little-game/server/src/main/scala/com/quane/little/game/server/LittleExtension.scala
@@ -3,6 +3,13 @@ package com.quane.little.game.server
 import com.smartfoxserver.v2.extensions.SFSExtension
 import com.quane.little.game.LittleGameEngine
 import com.quane.little.game.server.events.{MoveHandler, ReadyHandler, SpawnHandler}
+import com.smartfoxserver.v2.entities.variables.{SFSRoomVariable, RoomVariable}
+import com.smartfoxserver.v2.entities.User
+import com.quane.little.game.entity.{Entity, Mob}
+import com.smartfoxserver.v2.entities.data.{SFSObject, ISFSObject}
+import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConversions._
+import com.smartfoxserver.v2.SmartFoxServer
 
 /**
  * The SmartFox server 'extension' for the little game.
@@ -22,9 +29,54 @@ class LittleExtension
       + gameEngine.entities.length + " items and "
       + gameEngine.players.length + " mobs."
     )
+    gameEngine.update()
     addRequestHandler("ready", classOf[ReadyHandler])
     addRequestHandler("move", classOf[MoveHandler])
     addRequestHandler("spawn", classOf[SpawnHandler])
+
+
+    val user: User = null
+    val room = getParentRoom
+    trace("Initializing variables in " + room.getName)
+    val roomVariables = spawnEverything()
+    getApi.setRoomVariables(user, room, roomVariables, true, true, true)
+  }
+
+
+  private def spawnEverything(): List[RoomVariable] = {
+    val roomVariables = new ListBuffer[RoomVariable]
+    gameEngine.players foreach {
+      mob =>
+        trace("Creating variable for NPC: " + mob)
+        new SFSRoomVariable(mob.uuid.toString, toSFSObject(mob))
+      // TODO create variable
+    }
+    gameEngine.entities foreach {
+      entity =>
+        trace("Creating variable for item: " + entity)
+        new SFSRoomVariable(entity.uuid.toString, toSFSObject(entity))
+      // TODO create item
+
+    }
+    roomVariables.toList
+  }
+
+  private def toSFSObject(mob: Mob): ISFSObject = {
+    val data = new SFSObject()
+    data.putUtfString("id", mob.uuid.toString)
+    data.putFloat("x", mob.x)
+    data.putFloat("y", mob.y)
+    data.putInt("s", mob.speed)
+    data.putInt("d", mob.direction)
+    data
+  }
+
+  private def toSFSObject(mob: Entity): ISFSObject = {
+    val data = new SFSObject()
+    data.putUtfString("id", mob.uuid.toString)
+    data.putFloat("x", mob.x)
+    data.putFloat("y", mob.y)
+    data
   }
 
 }
