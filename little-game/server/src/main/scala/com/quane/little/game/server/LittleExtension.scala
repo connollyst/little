18ba23751b033f7e8bdbb7edda01e9f58d060d42
Sean Connolly <connolly.st@gmail.com>
2014-03-16 15:49:12 +0100

    Removed unused request/event handlers. Added warning if room is not an MMORoom.

--- little-game/server/src/main/scala/com/quane/little/game/server/LittleExtension.scala
+++ little-game/server/src/main/scala/com/quane/little/game/server/LittleExtension.scala
@@ -1,15 +1,8 @@
 package com.quane.little.game.server
 
-import com.smartfoxserver.v2.extensions.SFSExtension
+import com.smartfoxserver.v2.extensions.{ExtensionLogLevel, SFSExtension}
 import com.quane.little.game.LittleGameEngine
-import com.quane.little.game.server.events._
-import com.smartfoxserver.v2.entities.variables.{SFSRoomVariable, RoomVariable}
-import com.smartfoxserver.v2.entities.User
-import com.quane.little.game.entity.Entity
-import com.smartfoxserver.v2.entities.data.{SFSObject, ISFSObject}
-import scala.collection.mutable.ListBuffer
-import scala.collection.JavaConversions._
-import com.smartfoxserver.v2.core.SFSEventType
+import com.smartfoxserver.v2.mmo.MMORoom
 
 /**
  * The SmartFox server 'extension' for the little game.
@@ -22,41 +15,20 @@ class LittleExtension
   var gameEngine: LittleGameEngine = null
 
   override def init(): Unit = {
-    trace("Hello Little World!")
+    trace("Initializing Little room..")
+    if (!isMMORoom) {
+      trace(ExtensionLogLevel.ERROR, "Not configured to be an MMO room!")
+    }
     gameEngine = new LittleGameEngine
     gameEngine.initialize()
     trace("Initialized with "
       + gameEngine.entities.size + " items and "
       + gameEngine.players.size + " mobs."
     )
-    addRequestHandler("ready", classOf[ReadyHandler])
-    addEventHandler(SFSEventType.USER_JOIN_ROOM, classOf[JoinEventHandler])
   }
 
-  def setRoomVariables(): Unit = {
-    trace("Setting room variables..")
-    val user: User = null // the server
-    val room = getParentRoom
-    getApi.setRoomVariables(user, room, roomVariables)
-  }
-
-  private def roomVariables: List[RoomVariable] = {
-    val roomVariables = new ListBuffer[RoomVariable]
-    gameEngine.entities.keys foreach {
-      uuid =>
-        val entity = gameEngine.entities(uuid)
-        trace("Creating room variable for item: " + entity)
-        new SFSRoomVariable(uuid, toSFSObject(entity))
-    }
-    roomVariables.toList
-  }
-
-  private def toSFSObject(mob: Entity): ISFSObject = {
-    val data = new SFSObject()
-    data.putUtfString("id", mob.uuid.toString)
-    data.putFloat("x", mob.x)
-    data.putFloat("y", mob.y)
-    data
+  private def isMMORoom: Boolean = {
+    getParentRoom.isInstanceOf[MMORoom]
   }
 
 }
