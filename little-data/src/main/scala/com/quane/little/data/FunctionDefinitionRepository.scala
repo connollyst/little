d2a786f0ac37c7cc7be67dbe5270d1f67972c9f6
Sean Connolly <connolly.st@gmail.com>
2014-03-30 00:47:59 +0100

    Added skeleton of little-data module.

---
+++ little-data/src/main/scala/com/quane/little/data/FunctionDefinitionRepository.scala
@@ -0,0 +1,18 @@
+package com.quane.little.data
+
+import com.quane.little.language.FunctionDefinition
+import scala.collection.mutable
+
+/** Provides storage and retrieval access to the repository of [[com.quane.little.language.FunctionDefinition]] objects.
+  *
+  * @author Sean Connolly
+  */
+class FunctionDefinitionRepository {
+
+  private val funs: mutable.Map[String, FunctionDefinition] = mutable.Map()
+
+  def fetch(id: String): FunctionDefinition = funs(id)
+
+  def save(id: String, fun: FunctionDefinition): Unit = funs += (id -> fun)
+
+}
