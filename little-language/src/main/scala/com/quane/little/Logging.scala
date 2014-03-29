27130b8fa7a760f514e7cb02199e4ef159fa9273
Sean Connolly <connolly.st@gmail.com>
2014-03-29 18:37:37 +0100

    Reduced visibility on logging functions.

--- little-language/src/main/scala/com/quane/little/Logging.scala
+++ little-language/src/main/scala/com/quane/little/Logging.scala
@@ -6,20 +6,20 @@ trait Logging {
 
   private val logger = LoggerFactory.getLogger(getClass)
 
-  def debug(message: String) = logger.debug(message)
+  protected def debug(message: String) = logger.debug(message)
 
-  def info(message: String) = logger.info(message)
+  protected def info(message: String) = logger.info(message)
 
-  def warn(message: String) = logger.warn(message)
+  protected def warn(message: String) = logger.warn(message)
 
-  def error(message: String) = logger.error(message)
+  protected def error(message: String) = logger.error(message)
 
-  def debug(message: String, cause: Exception) = logger.debug(message, cause)
+  protected def debug(message: String, cause: Exception) = logger.debug(message, cause)
 
-  def info(message: String, cause: Exception) = logger.info(message, cause)
+  protected def info(message: String, cause: Exception) = logger.info(message, cause)
 
-  def warn(message: String, cause: Exception) = logger.warn(message, cause)
+  protected def warn(message: String, cause: Exception) = logger.warn(message, cause)
 
-  def error(message: String, cause: Exception) = logger.error(message, cause)
+  protected def error(message: String, cause: Exception) = logger.error(message, cause)
 
 }
