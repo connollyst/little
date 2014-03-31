ad6bad1ddf3fec3e9b94d36bb5ee38195476f2fb
Sean Connolly <connolly.st@gmail.com>
2014-03-31 21:09:53 +0200

    Removed unused commented code.

--- little-language/src/main/scala/com/quane/little/language/Expression.scala
+++ little-language/src/main/scala/com/quane/little/language/Expression.scala
@@ -1,8 +1,7 @@
 package com.quane.little.language
 
-import com.quane.little.language.data.Value
 import com.google.common.base.Objects
-
+import com.quane.little.language.data.Value
 
 /** An expression is one of the lowest level components of the language. It has a
   * return type and can be evaluated.
@@ -11,7 +10,6 @@ import com.google.common.base.Objects
   */
 trait Expression {
 
-  // [T <: Value]
   def evaluate: Value
 
   override def toString: String =
