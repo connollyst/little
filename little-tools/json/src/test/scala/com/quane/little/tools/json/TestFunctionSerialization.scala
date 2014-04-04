8d1bd5e56f5ecf602111c306e9602bf43d6cb64d
Sean Connolly <connolly.st@gmail.com>
2014-04-04 23:49:07 +0200

    FunctionReference JSON serialization tests.

--- little-tools/json/src/test/scala/com/quane/little/tools/json/TestFunctionSerialization.scala
+++ little-tools/json/src/test/scala/com/quane/little/tools/json/TestFunctionSerialization.scala
@@ -1,6 +1,7 @@
 package com.quane.little.tools.json
 
 import com.quane.little.language._
+import com.quane.little.language.data.Value
 import com.quane.little.tools.json.JSONTestUtilities._
 import org.junit.runner.RunWith
 import org.scalatest.FlatSpec
@@ -15,13 +16,40 @@ import org.scalatest.matchers.ShouldMatchers
 @RunWith(classOf[JUnitRunner])
 class TestFunctionSerialization extends FlatSpec with ShouldMatchers {
 
-  "json serializer" should "serialize function reference" in {
+  /* Function References */
+
+  "json serializer" should "serialize blank function reference" in {
     val name = "function_reference_blank"
     val fun = new FunctionReference(name)
     assertSerialization(getJSON(name), fun)
   }
 
-  it should "serialize function definition" in {
+  it should "serialize function reference with single value argument" in {
+    val name = "function_reference_value_single"
+    val fun = new FunctionReference(name)
+    fun.addArg("x", Value("abc"))
+    assertSerialization(getJSON(name), fun)
+  }
+
+  it should "serialize function reference with multiple value arguments" in {
+    val name = "function_reference_value_multiple"
+    val fun = new FunctionReference(name)
+    fun.addArg("x", Value("abc"))
+    fun.addArg("y", Value(123))
+    fun.addArg("z", Value(true))
+    assertSerialization(getJSON(name), fun)
+  }
+
+  it should "serialize function reference with function reference argument" in {
+    val name = "function_reference_with_function_reference"
+    val fun = new FunctionReference(name)
+    fun.addArg("x", new FunctionReference("inner_function"))
+    assertSerialization(getJSON(name), fun)
+  }
+
+  /* Function Definitions */
+
+  it should "serialize blank function definition" in {
     val name = "function_definition_blank"
     val fun = new FunctionDefinition(name)
     val actual = new JSONSerializer().serialize(fun)
