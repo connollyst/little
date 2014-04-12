ce4ed7a1ce602ce2b431cdf68b2bc1472b3b3a9d
Sean Connolly <connolly.st@gmail.com>
2014-04-12 13:38:26 +0200

    Removed NotYetImplementedException so we can keep working.

--- little-ide/html/src/main/scala/com/quane/little/ide/view/html/CloseButton.scala
+++ little-ide/html/src/main/scala/com/quane/little/ide/view/html/CloseButton.scala
@@ -1,13 +1,12 @@
 package com.quane.little.ide.view.html
 
-import com.quane.little.language.exceptions.NotImplementedError
 import com.vaadin.ui.Button
 
 object CloseButton {
 
   def apply(c: RemovableComponent) = new CloseButton {
     //    clickListeners += c.removeFromParent()
-    throw new NotImplementedError("Sean, you've been drinking.")
+    println("Sean, you've been drinking.")
   }
 
 }
