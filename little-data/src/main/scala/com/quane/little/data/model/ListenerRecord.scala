cb7b46726acc5e6035c99c2eb3e08f2de488b204
Sean Connolly <connolly.st@gmail.com>
2014-04-12 15:13:13 +0200

    Flushed out ListenerRecord a bit.

--- little-data/src/main/scala/com/quane/little/data/model/ListenerRecord.scala
+++ little-data/src/main/scala/com/quane/little/data/model/ListenerRecord.scala
@@ -1,5 +1,6 @@
 package com.quane.little.data.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.google.common.base.Objects
 import com.quane.little.language.event.EventListener
 
@@ -7,14 +8,29 @@ import com.quane.little.language.event.EventListener
   *
   * @author Sean Connolly
   */
-class ListenerRecord(var definition: EventListener) {
+class ListenerRecord(val ownerId: RecordId, var listener: EventListener)
+  extends HasRecordId {
 
-  var _id: RecordId = _
+  @JsonProperty("_id")
+  var id: RecordId = _
+
+  override def equals(other: Any): Boolean = other match {
+    case that: ListenerRecord =>
+      id == that.id &&
+        ownerId == that.ownerId &&
+        listener == that.listener
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(id, ownerId, listener)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
 
   override def toString: String =
     Objects.toStringHelper(getClass)
-      .add("id", _id)
-      .add("definition", definition)
+      .add("id", id)
+      .add("listener", listener)
       .toString
 
 }
