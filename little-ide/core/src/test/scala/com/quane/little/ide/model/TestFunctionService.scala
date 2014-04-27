a8055c07f67a550d5405a5dfebf8197788fa0450
Sean Connolly <connolly.st@gmail.com>
2014-04-27 23:42:58 +0200

    Added test cases for FunctionService

---
+++ little-ide/core/src/test/scala/com/quane/little/ide/model/TestFunctionService.scala
@@ -0,0 +1,51 @@
+package com.quane.little.ide.model
+
+import org.scalatest.FlatSpec
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import com.quane.little.language.FunctionDefinition
+import com.mongodb.casbah.MongoClient
+
+/** Test cases for the [[FunctionService]]
+  *
+  * @author Sean Connolly
+  */
+@RunWith(classOf[JUnitRunner])
+class TestFunctionService extends FlatSpec with EmbeddedMongoDB with ShouldMatchers {
+
+  val client = MongoClient(mongoHost, mongoPort)
+  val users = UserService(client)
+  val functions = FunctionService(client)
+
+  val userOne = "userOne"
+  val userTwo = "userTwo"
+
+  override def beforeAll() {
+    super.beforeAll()
+    users.upsert(userOne)
+    users.upsert(userTwo)
+    // TODO if we always initialize, we can't test initialization..
+    users.init()
+    functions.init()
+  }
+
+  "FunctionService" should "should detect existing function" in {
+    val name = "MyFunction"
+    functions.insert(userOne, new FunctionDefinition(name))
+    val exists = functions.exists(userOne, name)
+    exists should be(right = true)
+  }
+  it should "should not detect non-existing function" in {
+    val name = "UnknownFunction"
+    val exists = functions.exists(userOne, name)
+    exists should be(right = false)
+  }
+  it should "should not detect other user's existing function" in {
+    val name = "HisFunction"
+    functions.insert(userTwo, new FunctionDefinition(name))
+    val exists = functions.exists(userOne, name)
+    exists should be(right = false)
+  }
+
+}
