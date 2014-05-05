0eb6cbba7d7b3f122974046c2cdb724c59370e71
Sean Connolly <connolly.st@gmail.com>
2014-05-05 23:21:58 +0200

    Workspace comments and refactoring.

--- little-ide/html/src/main/scala/com/quane/little/ide/view/html/WorkspaceLayout.scala
+++ little-ide/html/src/main/scala/com/quane/little/ide/view/html/WorkspaceLayout.scala
@@ -1,7 +1,7 @@
 package com.quane.little.ide.view.html
 
 import com.quane.little.ide.presenter.FunctionDefinitionPresenter
-import com.quane.little.ide.view.WorkspaceView
+import com.quane.little.ide.view.{WorkspaceViewPresenter, WorkspaceView}
 import com.quane.vaadin.scala.{DroppableTarget, VaadinMixin}
 import com.vaadin.ui.{CssLayout, HorizontalLayout}
 import com.vaadin.event.dd.{DragAndDropEvent, DropHandler}
@@ -13,6 +13,10 @@ object WorkspaceLayout {
   val Style = "l-workspace"
 }
 
+/** The workspace is the tabletop on which the user develops their code.
+  *
+  * @author Sean Connolly
+  */
 class WorkspaceLayout
   extends CssLayout
   with WorkspaceView
@@ -23,17 +27,7 @@ class WorkspaceLayout
   setStyleName(WorkspaceLayout.Style)
   val workspace = new DroppableTarget(new HorizontalLayout())
   workspace.component.setSpacing(true)
-  workspace.setDropHandler(new DropHandler {
-    override def getAcceptCriterion = AcceptAll.get()
-
-    override def drop(event: DragAndDropEvent) =
-      event.getTransferable match {
-        case transferable: FunctionTransferable =>
-          IDECommandExecutor.execute(
-            new AddFunctionDefinitionCommand(presenter, transferable.functionId)
-          )
-      }
-  })
+  workspace.setDropHandler(new WorkspaceDropHandler(presenter))
   workspace.setSizeFull()
   add(workspace)
 
@@ -42,5 +36,24 @@ class WorkspaceLayout
     workspace.component.addComponent(view)
     new FunctionDefinitionPresenter(view)
   }
+
+}
+
+/**
+ * Handler for drag &amp; drop events onto the workspace.
+ *
+ * @param presenter the workspace presenter to interact with
+ */
+class WorkspaceDropHandler(presenter: WorkspaceViewPresenter) extends DropHandler {
+
+  override def getAcceptCriterion = AcceptAll.get()
+
+  override def drop(event: DragAndDropEvent) =
+    event.getTransferable match {
+      case transferable: FunctionTransferable =>
+        IDECommandExecutor.execute(
+          new AddFunctionDefinitionCommand(presenter, transferable.functionId)
+        )
+    }
 
 }
