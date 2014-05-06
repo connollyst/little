a44bdcd4e0319bc0cc66bd79f21244ccaeb4f8de
Sean Connolly <connolly.st@gmail.com>
2014-05-06 19:40:08 +0200

    Refactoring & documentation.

--- little-game/core/src/main/scala/com/quane/little/game/physics/PhysicalContactListener.scala
+++ little-game/core/src/main/scala/com/quane/little/game/physics/PhysicalContactListener.scala
@@ -3,39 +3,42 @@ package com.quane.little.game.physics
 import com.quane.little.game.entity.Entity
 import com.quane.little.language.event.LittleEvent
 import com.quane.little.tools.Logging
-import java.lang.Override
 import org.jbox2d.callbacks.{ContactImpulse, ContactListener}
 import org.jbox2d.collision.Manifold
 import org.jbox2d.dynamics.Body
 import org.jbox2d.dynamics.contacts.Contact
 
+/** A Box2D contact listener which translates physical body and sensor
+  * interactions in the simulation to [[Entity]] level interactions.
+  *
+  * @author Sean Connolly
+  */
 class PhysicalContactListener
   extends ContactListener
   with Logging {
 
-  @Override
-  override def preSolve(contact: Contact, manifold: Manifold) {
+  override def preSolve(contact: Contact, manifold: Manifold) = {
     // do nothing
   }
 
-  @Override
-  override def postSolve(contact: Contact, impulse: ContactImpulse) {
+  override def postSolve(contact: Contact, impulse: ContactImpulse) = {
     // do nothing
   }
 
-  @Override
-  override def beginContact(contact: Contact) {
-    report(LittleEvent.OnContact, contact)
-  }
+  override def beginContact(contact: Contact) =
+    reportInteraction(LittleEvent.OnContact, contact)
 
-  @Override
-  override def endContact(contact: Contact) {
-    report(LittleEvent.OnContactEnded, contact)
-  }
+  override def endContact(contact: Contact) =
+    reportInteraction(LittleEvent.OnContactEnded, contact)
 
-  private def report(event: LittleEvent, contact: Contact) {
-    val fixtureA = contact.getFixtureA
-    val fixtureB = contact.getFixtureB
+  /** Report the interaction event.
+    *
+    * @param event the interaction event type
+    * @param interaction the physical interaction data
+    */
+  private def reportInteraction(event: LittleEvent, interaction: Contact) {
+    val fixtureA = interaction.getFixtureA
+    val fixtureB = interaction.getFixtureB
     val bodyA = fixtureA.getBody
     val bodyB = fixtureB.getBody
     val entityA = getContactEntity(bodyA)
@@ -43,28 +46,50 @@ class PhysicalContactListener
     if (fixtureA.isSensor && fixtureB.isSensor) {
       // Two sensors do not substantiate an interaction
     } else if (fixtureA.isSensor || fixtureB.isSensor) {
-      // One sensor and one entity
-      reportProximity(entityA, entityB)
+      // Proximity: one sensor and one entity
+      reportProximity(event, entityA, entityB)
     } else {
-      // Two entities
-      reportContact(entityA, entityB)
+      // Contact: two entities
+      reportContact(event, entityA, entityB)
     }
   }
 
-  private def reportProximity(entity: Entity, sensor: Entity) {
-    debug("Proximity: " + entity.id + " & " + sensor.id)
-    sensor.approachedBy(entity)
-    entity.approachedBy(sensor)
+  /** Report a proximity interaction event.<br/>
+    * Proximity is defined by one entity interacting with the sensor of another.
+    * <p>
+    * <b>Note:</b> interactions are equally bi-directional
+    * </p>
+    *
+    * @param event the interaction event type
+    * @param entityA one of the interacting Entities
+    * @param entityB the other interacting Entity
+    */
+  private def reportProximity(event: LittleEvent, entityA: Entity, entityB: Entity) {
+    if (!entityA.isRemoved && !entityB.isRemoved) {
+      // TODO handle proximity ending
+      entityB.approachedBy(entityA)
+      entityA.approachedBy(entityB)
+    }
   }
 
-  private def reportContact(entityA: Entity, entityB: Entity) {
-    debug("Contact: " + entityA.id + " & " + entityB.id)
-    entityA.touchedBy(entityB)
-    entityB.touchedBy(entityA)
+  /** Report a contact interaction event.<br/>
+    * Contact is defined by one entity interacting directly with another.
+    * <p>
+    * <b>Note:</b> interactions are equally bi-directional
+    * </p>
+    *
+    * @param event the interaction event type
+    * @param entityA one of the interacting Entities
+    * @param entityB the other interacting Entity
+    */
+  private def reportContact(event: LittleEvent, entityA: Entity, entityB: Entity) {
+    if (!entityA.isRemoved && !entityB.isRemoved) {
+      // TODO handle contact ending
+      entityA.touchedBy(entityB)
+      entityB.touchedBy(entityA)
+    }
   }
 
-  private def getContactEntity(body: Body): Entity = {
-    body.getUserData.asInstanceOf[Entity]
-  }
+  private def getContactEntity(body: Body): Entity = body.getUserData.asInstanceOf[Entity]
 
 }
