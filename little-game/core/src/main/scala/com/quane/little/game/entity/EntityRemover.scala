19cac51973512bdccf513be835d47e6a3fe4ef5d
Sean Connolly <connolly.st@gmail.com>
2014-05-06 18:55:25 +0200

    Added EntityRemover documentation and debugging output.

--- little-game/core/src/main/scala/com/quane/little/game/entity/EntityRemover.scala
+++ little-game/core/src/main/scala/com/quane/little/game/entity/EntityRemover.scala
@@ -3,8 +3,9 @@ package com.quane.little.game.entity
 import com.quane.little.tools.Logging
 import scala.collection.mutable.ListBuffer
 
-/** The remover is responsible for removing entities from the game and the
-  * physics engine.
+/** Responsible for removing Entities from the game.
+  *
+  * @author Sean Connolly
   */
 class EntityRemover
   extends Logging {
@@ -14,14 +15,20 @@ class EntityRemover
 
   def add(listener: EntityRemovalListener) = listeners += listener
 
-  def remove(entity: Entity) = queue += entity
+  def remove(entity: Entity) = {
+    debug("Queued entity to be removed from simulation: " + entity)
+    queue += entity
+  }
 
   def cleanAll() {
     queue foreach (
       entity => {
+        debug("Marking as removed: " + entity)
         entity.isRemoved = true
         listeners.foreach {
-          listener => listener.entityRemoved(entity)
+          listener =>
+            debug("Informing listener that Entity was removed: " + listener)
+            listener.entityRemoved(entity)
         }
       })
     queue.clear()
@@ -29,8 +36,16 @@ class EntityRemover
 
 }
 
+/** Listener for when an Entity is removed from the game.
+  *
+  * @author Sean Connolly
+  */
 trait EntityRemovalListener {
 
+  /** Called when the Entity has been marked as removed.
+    *
+    * @param entity the Entity which has been removed
+    */
   def entityRemoved(entity: Entity)
 
 }
