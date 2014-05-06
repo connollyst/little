7b71c8a20475de4fb94b3c71eb988486e8475f26
Sean Connolly <connolly.st@gmail.com>
2014-05-06 19:12:16 +0200

    Renamed ItemSerializer to EntitySerializer (added docs and removed logging).

--- little-game/server/src/main/scala/com/quane/little/game/server/EntitySerializer.scala
+++ little-game/server/src/main/scala/com/quane/little/game/server/EntitySerializer.scala
@@ -1,17 +1,15 @@
 package com.quane.little.game.server
 
 import com.quane.little.game.entity.{WorldEdge, Entity, Mob}
-import com.quane.little.tools.Logging
 import com.smartfoxserver.v2.mmo.{Vec3D, MMOItemVariable, IMMOItemVariable}
 import scala.collection.mutable.ListBuffer
 
-/**
- *
- *
- * @author Sean Connolly
- */
-class ItemSerializer
-  extends Logging {
+/** Serializes [[Entity]] objects as SmartFox [[IMMOItemVariable]] objects to be
+  * sent to the client.
+  *
+  * @author Sean Connolly
+  */
+class EntitySerializer {
 
   def serialize(entity: Entity): List[IMMOItemVariable] = {
     entity match {
@@ -22,7 +20,6 @@ class ItemSerializer
   }
 
   def serialize(position: Vec3D): List[IMMOItemVariable] = {
-    debug("Serializing position: " + position)
     val variables = new ListBuffer[IMMOItemVariable]
     variables += new MMOItemVariable("x", position.floatX())
     variables += new MMOItemVariable("y", position.floatY())
@@ -30,7 +27,6 @@ class ItemSerializer
   }
 
   private def serializeMob(mob: Mob): List[IMMOItemVariable] = {
-    debug("Serializing Mob MMO item: " + mob)
     val variables = new ListBuffer[IMMOItemVariable]
     variables += new MMOItemVariable("type", "player")
     variables += new MMOItemVariable("id", mob.id)
@@ -40,7 +36,6 @@ class ItemSerializer
   }
 
   private def serializeEntity(entity: Entity): List[IMMOItemVariable] = {
-    debug("Serializing Entity MMO item: " + entity)
     val variables = new ListBuffer[IMMOItemVariable]
     variables += new MMOItemVariable("type", "entity")
     variables += new MMOItemVariable("id", entity.id)
@@ -48,7 +43,6 @@ class ItemSerializer
   }
 
   private def serializeWorldEdge(wall: WorldEdge): List[IMMOItemVariable] = {
-    debug("Serializing Wall MMO item: " + wall)
     val variables = new ListBuffer[IMMOItemVariable]
     variables += new MMOItemVariable("type", "wall")
     variables += new MMOItemVariable("id", wall.id)
