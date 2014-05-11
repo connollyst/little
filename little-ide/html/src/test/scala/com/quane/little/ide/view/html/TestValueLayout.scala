e5fb446278898183b17869dd47d75a83bb63cbcf
Sean Connolly <connolly.st@gmail.com>
2014-05-11 20:18:12 +0200

    Switched test to use ShouldMatchers

--- little-ide/html/src/test/scala/com/quane/little/ide/view/html/TestValueLayout.scala
+++ little-ide/html/src/test/scala/com/quane/little/ide/view/html/TestValueLayout.scala
@@ -2,28 +2,27 @@ package com.quane.little.ide.view.html
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.FunSuite
+import org.scalatest.FlatSpec
 import org.scalatest.mock.MockitoSugar
 import org.mockito.Mockito._
 import com.quane.little.ide.view.html.InteractionSimulator._
 import com.quane.little.ide.view.ValueViewPresenter
+import org.scalatest.matchers.ShouldMatchers
 
 @RunWith(classOf[JUnitRunner])
-class TestValueLayout extends FunSuite with MockitoSugar {
+class TestValueLayout extends FlatSpec with ShouldMatchers with MockitoSugar {
 
-  test("should propagate changing value to presenter") {
+  "ValueLayout" should "propagate changing value to presenter" in {
     val view = new ValueLayout
     val presenter = mock[ValueViewPresenter]
     view.registerViewPresenter(presenter)
     setText(view.valueField, "hello world")
     verify(presenter).onValueChange("hello world")
   }
-
-  test("should change textbox text on setValue") {
+  it should "change textbox text on setValue" in {
     val view = new ValueLayout
     view.setValue("hello world")
     assert(view.valueField.getValue == "hello world")
   }
-
 
 }
