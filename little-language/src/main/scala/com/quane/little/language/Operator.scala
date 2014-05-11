88d3cbb26ea9d8ca034eb12f4f270a10a8b1b6e3
Sean Connolly <connolly.st@gmail.com>
2014-05-11 18:08:18 +0200

    Improved some old (bad) Scala code.

--- little-language/src/main/scala/com/quane/little/language/Operator.scala
+++ little-language/src/main/scala/com/quane/little/language/Operator.scala
@@ -2,58 +2,43 @@ package com.quane.little.language
 
 import com.quane.little.language.data.{Value, Variable}
 import com.quane.little.language.event.EventListener
-import com.quane.little.tools.Logging
 import scala.collection.mutable.ListBuffer
 import com.quane.little.language.event.Event.Event
 
 class Operator(override val runtime: Runtime, mob: Operable)
-  extends Scope(runtime)
-  with Logging {
+  extends Scope(runtime) {
 
   def x: Value = Value(mob.x toDouble)
 
   def y: Value = Value(mob.y toDouble)
 
-  def speed(speed: Int) = mob.speed = Math.max(Operable.MIN_SPEED, Math.min(speed, Operable.MAX_SPEED))
+  def speed(speed: Int) = mob.speed = scala.math.max(Operable.MIN_SPEED, scala.math.min(speed, Operable.MAX_SPEED))
 
   def speed: Int = mob.speed
 
-  def direction(degrees: Value) {
+  def direction(degrees: Value): Unit =
     if (degrees.asInt < 0) {
-      // TODO avoid creating a new Value here
+      // TODO can we avoid creating a new Value here?
       direction(Value(360 + degrees.asInt))
     } else {
       mob.direction = degrees.asInt % 360
     }
-  }
 
   def direction: Value = Value(mob.direction)
 
-  /* Listen for events */
+  val eventListeners = ListBuffer[EventListener]()
 
-  val eventListeners = new ListBuffer[EventListener]
+  def addEventListener(eventListener: EventListener) = eventListeners += eventListener
 
-  def addEventListener(eventListener: EventListener) {
-    debug("Listening for " + eventListener.event.getClass.getSimpleName + " events.")
-    eventListeners += eventListener
-  }
-
-  def getEventListeners(event: Event): List[EventListener] = {
-    val listening = new ListBuffer[EventListener]
-    eventListeners.foreach(
-      listener =>
-        if (listener.event == event) {
-          listening += listener
-        }
-    )
-    listening.toList
-  }
+  def getEventListeners(event: Event): Iterable[EventListener] =
+    eventListeners.filter {
+      listener => listener.event == event
+    }
 
   /* Intercept the Scope memory functions to access keyword variables */
 
-  override def save(variable: Variable) {
-    val name = variable.name
-    name match {
+  override def save(variable: Variable) =
+    variable.name match {
       case Operable.X => throw new IllegalAccessException("Cannot set X")
       case Operable.Y => throw new IllegalAccessException("Cannot set Y")
       case Operable.SPEED => speed(variable.value.asInt)
@@ -62,10 +47,9 @@ class Operator(override val runtime: Runtime, mob: Operable)
         // It's not a special variable, store it in normal memory
         super.save(variable)
     }
-  }
 
-  override def fetch(name: String): Variable = {
-    val variable = name match {
+  override def fetch(name: String): Variable =
+    name match {
       case Operable.X => new Variable(name, x)
       case Operable.Y => new Variable(name, y)
       case Operable.SPEED => new Variable(name, Value(mob.speed))
@@ -74,7 +58,5 @@ class Operator(override val runtime: Runtime, mob: Operable)
         // It's not a special variable, fetch it from normal memory
         super.fetch(name)
     }
-    variable
-  }
 
 }
