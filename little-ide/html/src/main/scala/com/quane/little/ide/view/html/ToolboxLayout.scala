1c866d199029307230ab46a037c332506e8ed9d4
Sean Connolly <connolly.st@gmail.com>
2014-05-18 17:04:17 +0200

    Consolidated the ToolboxItem class ino the ToolboxLayout file.

--- little-ide/html/src/main/scala/com/quane/little/ide/view/html/ToolboxLayout.scala
+++ little-ide/html/src/main/scala/com/quane/little/ide/view/html/ToolboxLayout.scala
@@ -1,18 +1,24 @@
 package com.quane.little.ide.view.html
 
 import com.quane.little.ide.view.ToolboxView
-import com.vaadin.ui.Accordion
+import com.vaadin.ui.{Label, DragAndDropWrapper, Accordion}
 import scala.collection.mutable
 import com.quane.little.data.model.RecordId
-import com.vaadin.ui.TabSheet.Tab
 import com.quane.little.data.model.CodeSubcategory.CodeSubcategory
 import com.quane.little.data.model.CodeCategory.CodeCategory
+import java.util
+import com.quane.little.ide.view.html.dnd.CodeTransferable
+import com.vaadin.ui.TabSheet.Tab
 
 object ToolboxLayout {
   val Style = "l-toolbox"
   val StyleAccordion = Style + "-accordion"
 }
 
+/** An HTML layout view for the toolbox of code the user can work with.
+  *
+  * @author Sean Connolly
+  */
 class ToolboxLayout
   extends Accordion
   with ToolboxView
@@ -30,15 +36,14 @@ class ToolboxLayout
     addTab(subcategory, new ToolboxSectionComponent())
 
   override def createToolboxItem(category: CodeCategory, subcategory: CodeSubcategory, title: String, functionId: RecordId) =
-    addToolboxItem(subcategory, new ToolboxItemComponent(title, category, functionId))
+    addToolboxItem(subcategory, new ToolboxItem(title, category, functionId))
 
-  private def addToolboxItem(subcategory: CodeSubcategory, item: ToolboxItemComponent) = {
+  private def addToolboxItem(subcategory: CodeSubcategory, item: ToolboxItem) = {
     val tab = getTab(subcategory)
     val content = getTabContents(subcategory)
     content.add(item)
     tab.setCaption(subcategory.toString + " (" + content.count + ")")
   }
-
 
   private def addTab(subcategory: CodeSubcategory, tabContent: ToolboxSectionComponent): Unit = {
     val id = subcategory.toString
@@ -52,5 +57,25 @@ class ToolboxLayout
 
   private def getTabContents(subcategory: CodeSubcategory): ToolboxSectionComponent =
     tabContents(subcategory.toString)
+
+}
+
+/** An item in the toolbox.
+  *
+  * @param title the item title
+  * @param category the code category
+  * @param codeId the id for the represented code
+  */
+class ToolboxItem(val title: String, val category: CodeCategory, val codeId: RecordId)
+  extends DragAndDropWrapper(new Label(title)) {
+
+  setSizeUndefined()
+  setStyleName(ExpressionLayout.Style)
+  setDragStartMode(DragAndDropWrapper.DragStartMode.WRAPPER)
+
+  // TODO should look something like the real expression/statement
+
+  override def getTransferable(rawVariables: util.Map[String, AnyRef]) =
+    new CodeTransferable(this, category, codeId)
 
 }
