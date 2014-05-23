2e291afd1c3ab10901df6df9aaeb3b426d00290a
Sean Connolly <connolly.st@gmail.com>
2014-05-24 00:28:07 +0200

    Corrected tests for ConditionalPresenter.

--- little-ide/core/src/test/scala/com/quane/little/ide/presenter/TestConditionalPresenter.scala
+++ little-ide/core/src/test/scala/com/quane/little/ide/presenter/TestConditionalPresenter.scala
@@ -2,21 +2,43 @@ package com.quane.little.ide.presenter
 
 import org.scalatest.FunSuite
 import org.scalatest.mock.MockitoSugar
-import com.quane.little.ide.view.ConditionalView
+import com.quane.little.ide.view._
 import org.mockito.Mockito._
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import com.quane.little.ide.MockIDEBindingModule
 
+/** Test cases for the [[com.quane.little.ide.presenter.ConditionalPresenter]].
+  *
+  * @author Sean Connolly
+  */
 @RunWith(classOf[JUnitRunner])
 class TestConditionalPresenter extends FunSuite with MockitoSugar {
 
   implicit val bindingModule = MockIDEBindingModule
 
   test("test listener registered") {
-    val view = mock[ConditionalView]
+    val view = mockConditionalView
     val presenter = new ConditionalPresenter(view)
     verify(view).registerViewPresenter(presenter)
+  }
+
+  /** Utility function for mocking out a [[com.quane.little.ide.view.ConditionalView]]
+    * which returns instances of the appropriate [[com.quane.little.ide.view.MockView]]
+    * when asked to.
+    *
+    * @return the mock conditional view
+    */
+  private def mockConditionalView: ConditionalView = {
+    val view = mock[ConditionalView]
+    when(view.createThenBlock()).thenReturn(new MockBlockView)
+    when(view.createElseBlock()).thenReturn(new MockBlockView)
+    when(view.createMathCondition()).thenReturn(new MockMathView)
+    when(view.createGetterCondition()).thenReturn(new MockGetterView)
+    when(view.createLogicCondition()).thenReturn(new MockLogicView)
+    when(view.createConditionalCondition()).thenReturn(new MockConditionalView)
+    when(view.createFunctionReferenceCondition()).thenReturn(new MockFunctionReferenceView)
+    view
   }
 
 }
