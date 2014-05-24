5bb4df7c6f5eebc3f5d0befa00fefd639e515963
Sean Connolly <connolly.st@gmail.com>
2014-05-24 12:33:02 +0200

    Camera follows the player's character.

---
+++ little-game/client/Assets/Scripts/GameCamera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour
+{
+	
+		private float speed = 50;
+		private Transform target;
+	
+		public void SetTarget (Transform t)
+		{
+				target = t;
+				Debug.LogError ("Camera following " + t);
+		}
+	
+		void LateUpdate ()
+		{
+				if (target) {
+						float x = IncrementTowards (transform.position.x, target.position.x, speed);
+						float y = IncrementTowards (transform.position.y, target.position.y, speed);
+						transform.position = new Vector3 (x, y, transform.position.z);
+				}
+		}
+		
+		private float IncrementTowards (float n, float target, float a)
+		{
+				if (n == target) {
+						return n;
+				} else {
+						float dir = Mathf.Sign (target - n);
+						n += a * Time.deltaTime * dir;
+						return (dir == Mathf.Sign (target - n)) ? n : target;
+				}
+		}
+}
