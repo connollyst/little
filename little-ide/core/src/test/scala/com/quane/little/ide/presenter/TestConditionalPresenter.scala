ce2c4b5bac55b95e8cb6f90fb188230cee5b6bfc
Sean Connolly <connolly.st@gmail.com>
2014-10-01 22:47:57 +0200

    Simplified ConditionalViewPresenter test suite

--- little-ide/core/src/test/scala/com/quane/little/ide/presenter/TestConditionalPresenter.scala
+++ little-ide/core/src/test/scala/com/quane/little/ide/presenter/TestConditionalPresenter.scala
@@ -18,29 +18,11 @@ class TestConditionalPresenter extends WordSpec with MockitoSugar {
   implicit val bindingModule = MockIDEBindingModule
 
   "ConditionalPresenter" should {
-    "test listener registered" in {
-      val view = mockConditionalView
+    "register itself with its view" in {
+      val view = MockConditionalView.mocked()
       val presenter = new ConditionalPresenter(view)
       verify(view).registerViewPresenter(presenter)
     }
-  }
-
-  /** Utility function for mocking out a [[com.quane.little.ide.view.ConditionalView]]
-    * which returns instances of the appropriate [[com.quane.little.ide.view.MockView]]
-    * when asked to.
-    *
-    * @return the mock conditional view
-    */
-  private def mockConditionalView: ConditionalView = {
-    val view = mock[ConditionalView]
-    when(view.createThenBlock()).thenReturn(new MockBlockView)
-    when(view.createElseBlock()).thenReturn(new MockBlockView)
-    when(view.createMathCondition()).thenReturn(new MockMathView)
-    when(view.createGetterCondition()).thenReturn(new MockGetterView)
-    when(view.createLogicCondition()).thenReturn(new MockLogicView)
-    when(view.createConditionalCondition()).thenReturn(new MockConditionalView)
-    when(view.createFunctionReferenceCondition()).thenReturn(new MockFunctionReferenceView)
-    view
   }
 
 }
--- little-ide/core/src/test/scala/com/quane/little/ide/view/MockConditionalView.scala
+++ little-ide/core/src/test/scala/com/quane/little/ide/view/MockConditionalView.scala
@@ -1,7 +1,29 @@
 package com.quane.little.ide.view
 
-object MockConditionalView {
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+
+object MockConditionalView extends MockitoSugar {
+
   def apply(): MockConditionalView = new MockConditionalView
+
+  /** Utility function for mocking out a [[com.quane.little.ide.view.ConditionalView]]
+    * which returns instances of the appropriate mock views when asked to.
+    *
+    * @return the mock conditional view
+    */
+  def mocked(): ConditionalView = {
+    val view = mock[ConditionalView]
+    when(view.createThenBlock()).thenReturn(new MockBlockView)
+    when(view.createElseBlock()).thenReturn(new MockBlockView)
+    when(view.createMathCondition()).thenReturn(new MockMathView)
+    when(view.createGetterCondition()).thenReturn(new MockGetterView)
+    when(view.createLogicCondition()).thenReturn(new MockLogicView)
+    when(view.createConditionalCondition()).thenReturn(new MockConditionalView)
+    when(view.createFunctionReferenceCondition()).thenReturn(new MockFunctionReferenceView)
+    view
+  }
+
 }
 
 /** An implementation of [[com.quane.little.ide.view.ConditionalView]] for
