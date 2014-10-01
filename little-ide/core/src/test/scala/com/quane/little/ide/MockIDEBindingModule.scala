d7e32811ba935a248817a5b883d69fab5ff9133d
Sean Connolly <connolly.st@gmail.com>
2014-10-01 19:37:00 +0200

    Documented class.

--- little-ide/core/src/test/scala/com/quane/little/ide/MockIDEBindingModule.scala
+++ little-ide/core/src/test/scala/com/quane/little/ide/MockIDEBindingModule.scala
@@ -3,8 +3,8 @@ package com.quane.little.ide
 import com.escalatesoft.subcut.inject.NewBindingModule
 import com.quane.little.data.MockDataBindingModule
 
-/**
- *
+/** An extension of the normal IDE binding module which provides mock
+ * implementations of the data services for testing higher level function.
  *
  * @author Sean Connolly
  */
