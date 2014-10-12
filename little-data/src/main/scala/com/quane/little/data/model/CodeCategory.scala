40677d50da1db4a09eb8e2a6eb8c3f5c76452e9c
Sean Connolly <connolly.st@gmail.com>
2014-10-12 20:02:40 +0200

    Removed unused CodeType.EventListener

--- little-data/src/main/scala/com/quane/little/data/model/CodeCategory.scala
+++ little-data/src/main/scala/com/quane/little/data/model/CodeCategory.scala
@@ -7,7 +7,7 @@ package com.quane.little.data.model
 object CodeType extends Enumeration {
   type CodeType = Value
   // TODO this can be inferred by the class hierarchy!
-  val Primitive, Function, EventListener = Value
+  val Primitive, Function = Value
 }
 
 /** User to organize functions in the UI.
