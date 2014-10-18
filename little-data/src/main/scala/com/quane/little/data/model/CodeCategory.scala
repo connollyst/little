104ba9ded07aa08a2972955e8386600ca1916164
Sean Connolly <connolly.st@gmail.com>
2014-10-18 18:44:08 +0200

    Separated CodeType from CodeCategory

--- little-data/src/main/scala/com/quane/little/data/model/CodeCategory.scala
+++ little-data/src/main/scala/com/quane/little/data/model/CodeCategory.scala
@@ -1,15 +1,5 @@
 package com.quane.little.data.model
 
-/** Used to organize code in the UI.
-  *
-  * @author Sean Connolly
-  */
-object CodeType extends Enumeration {
-  type CodeType = Value
-  // TODO this can be inferred by the class hierarchy!
-  val Primitive, Function = Value
-}
-
 /** Used to organize functions in the UI.
   *
   * @author Sean Connolly
